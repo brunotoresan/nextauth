e309d03fc9938be6a7d8cef163002e8c1940c1e5
Bruno Ramos Toresan <brunotoresan@gmail.com>
2021-09-10 12:56:56 -0300

    retrieving authentication status

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { setCookie } from 'nookies'
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from 'nookies'
 import Router from 'next/router'
 import { api } from "../services/api";
 
@@ -30,6 +30,17 @@ export function AuthProvider({ children }: AuthProviderProps){
   const [user, setUser] = useState<User>(null)
   const isAuthenticated = !(!user)
 
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies()
+
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data
+        setUser({ email, permissions, roles })
+      })
+    }
+  }, [])
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('sessions', {
@@ -48,13 +59,14 @@ export function AuthProvider({ children }: AuthProviderProps){
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/' // cookie avilable globally
       })
-
  
       setUser({
         email,
         permissions,
         roles
       })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       Router.push('/dashboard')
     } catch(err) {
